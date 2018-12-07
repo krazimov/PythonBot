3b6e0e126b3a2711d353764ce60f05442ee6b9cb
Rodrigo De la Rivera <crdelarivera@gmail.com>
2018-12-07 12:39:47 +0100

    Function to check dependencies

--- Bot.py
+++ Bot.py
@@ -7,6 +7,15 @@ import Sea
 def main():
     print 'Bot start'
     print 'Bot end'
+def checkDependencies():
+    from sys import modules
+
+    depends = ['cv2', 'numpy', 'win32api', 'win32con', 'win32gui', 'win32ui']
+    missing = [dep for dep in depends if dep not in modules]
+    if missing:
+        print 'Error, dependencies not found: {}'.format(', '.join(missing))
+        print 'Exiting program'
+        raise SystemExit
 
 if __name__ == '__main__':
     main()
